ece63b1c5837a75617751e11597e2fb29c1ec203
krabhiit <krabhiit@gmail.com>
2026-01-20 16:40:26 +0530

    Update Changes

--- secrets_manager_root/outputs.tf
+++ secrets_manager_root/outputs.tf
@@ -1,9 +1,1 @@
-output "secret_arn" {   
-    description = "ARN of the managed secret"
-    value       = module.secrets_manager.secret_arn
-}
-
-output "secret_name" {
-    description = "Name of the managed secret"
-    value       = module.secrets_manager.secret_name
-}
+#
