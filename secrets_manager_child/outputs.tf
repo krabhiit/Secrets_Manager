11793ebf775bb9be1857cd0f0c795da303d5e110
krabhiit <krabhiit@gmail.com>
2026-01-21 13:12:58 +0530

    Update Changes

--- secrets_manager_child/outputs.tf
+++ secrets_manager_child/outputs.tf
@@ -1,7 +1,7 @@
-output "secret_arn" {
-  value = var.create_new_secret? aws_secretsmanager_secret.this[0].arn: data.aws_secretsmanager_secret.existing[0].arn
+output "secret_name" {
+  value = var.create_new_secret? aws_secretsmanager_secret.new_secret[0].name: data.aws_secretsmanager_secret.existing_secret[0].name
 }
 
-output "secret_name" {
-  value = var.create_new_secret? aws_secretsmanager_secret.this[0].name: data.aws_secretsmanager_secret.existing[0].name
+output "secret_arn" {
+  value = var.create_new_secret? aws_secretsmanager_secret.new_secret[0].arn: data.aws_secretsmanager_secret.existing_secret[0].arn
 }
