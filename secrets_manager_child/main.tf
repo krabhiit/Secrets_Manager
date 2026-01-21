7fc334cca0f07f9f180021e1ef2e25e03e0fa913
krabhiit <krabhiit@gmail.com>
2026-01-21 20:46:50 +0530

    Update Changes

--- secrets_manager_child/main.tf
+++ secrets_manager_child/main.tf
@@ -16,9 +16,9 @@ resource "aws_secretsmanager_secret" "this" {
   description = var.description
   tags        = var.tags
 
-  lifecycle {
-    prevent_destroy = true
-  }
+#  lifecycle {
+#    prevent_destroy = true
+#  }
 }
 
 #############################
