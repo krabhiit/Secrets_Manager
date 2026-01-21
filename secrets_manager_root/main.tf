3c362a7c8c50c6bc5070f7233f02090836f1d7e3
krabhiit <krabhiit@gmail.com>
2026-01-21 20:31:05 +0530

    Update Changes

--- secrets_manager_root/main.tf
+++ secrets_manager_root/main.tf
@@ -1,10 +1,11 @@
 module "secrets_manager" {
-  source = "../secrets_manager_child"
+  for_each = var.secrets
+  source   = "../secrets_manager_child"
 
-  create_new_secret   = var.secret_config.create_new_secret
-  secret_name         = try(var.secret_config.secret_name, null)
-  description         = try(var.secret_config.description, null)
-  existing_secret_arn = try(var.secret_config.existing_secret_arn, null)
-  secret_string       = try(var.secret_config.secret_string, null)
-  tags                = try(var.secret_config.tags, {})
+  create_new_secret   = each.value.create_new_secret
+  secret_name         = lookup(each.value, "secret_name", null)
+  existing_secret_arn = lookup(each.value, "existing_secret_arn", null)
+  secret_string       = lookup(each.value, "secret_string", null)
+  description         = lookup(each.value, "description", null)
+  tags                = lookup(each.value, "tags", {})
 }
--- secrets_manager_root/variables.tf
+++ secrets_manager_root/variables.tf
@@ -1,11 +1,11 @@
-variable "secret_config" {
-  description = "Configuration for creating or updating a secret"
-  type = object({
+variable "secrets" {
+  description = "Secrets to create or update"
+  type = map(object({
     create_new_secret   = bool
     secret_name         = optional(string)
-    description         = optional(string)
     existing_secret_arn = optional(string)
     secret_string       = optional(string)
+    description         = optional(string)
     tags                = optional(map(string))
-  })
+  }))
 }
